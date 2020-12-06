d213248fb7a7774c0b9a2713f4d35a24936b69d9
William Bugenis <wbugenis@gmail.com>
2020-12-06 05:07:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,12 @@
-def using_concat()
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, element)
+  arr.insert(element, 4)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
